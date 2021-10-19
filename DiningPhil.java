820678f22c115529617b402f971159de51c6d2d4
Adysen <adysen66@gmail.com>
2021-10-19 14:31:05 -0400

    bones :-)

---
+++ DiningPhil.java
@@ -0,0 +1,46 @@
+public class DiningPhil {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+class Phil extends Thread {
+
+    private Fork = leftFork;
+    private Fork = rightFork;
+    private Strink name;
+
+    public Phil(String name, Fork left, Fork right) {
+        
+    }
+
+    public void think() {
+
+    }
+
+    public void eat() {
+
+    }
+
+    public void run() {
+
+    }
+}
+
+class Fork {
+    public String fork;
+    public boolean inUse;
+
+    public Fork(String fork) {
+        this.fork = fork;
+    }
+
+    public synchronized void pickUp() {
+
+    }
+
+    public synchronized putDown() {
+
+    }
+}
